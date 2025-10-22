1417354faea6206deea37096bc594a3d482b483c
Erica Box <boxey001@mymail.unisa.edu.au>
2025-10-22 21:55:49 +1030

    Completed Asset.py

--- Asset.py
+++ Asset.py
@@ -6,3 +6,36 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+
+class BaseAsset:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
+    def __str__(self):
+        if self.encrypted:
+            return f'{self.name}: {self.description} [encrypted]'
+        else:
+            return f'{self.name}: {self.description}'
+
+
+class CryptoToken(BaseAsset):
+    pass
+
+
+class DataSpike(BaseAsset):
+    pass
+
+
+class RemovableDrive(BaseAsset):
+    pass
+
+
+class SecurityChip(BaseAsset):
+    pass
+
+
+class HardwarePatch(BaseAsset):
+    pass
--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,13 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+import Rig
+
+
+class Hacker:
+    def __init__(self, name: str, inventory: list, rig: Rig,):
+        self.name = name: str
+        self.inventory = [CryptoToken]
+        self.rig = None
+        self.trace_level = 0
