c062f03a62307d4f374808e4e0d39bd787094fba
Erica Box <boxey001@mymail.unisa.edu.au>
2025-10-22 22:41:48 +1030

    Hacker completed, and acquire rig completed

--- Hacker.py
+++ Hacker.py
@@ -1,18 +1,27 @@
 """
 File: Hacker.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Defines the Rig class and related methods required for the Basic Programming Assignment.
+Author: Erica Box
+Author: Erica Box
+ID: 110468687
+Username: boxey001
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-import Rig
+from Rig import Rig
+from Asset import CryptoToken
 
 
 class Hacker:
-    def __init__(self, name: str, inventory: list, rig: Rig,):
-        self.name = name: str
-        self.inventory = [CryptoToken]
+    def __init__(self, name: str):
+        self.name = name
+        self.inventory = [CryptoToken("CryptoToken", "Used to acquire rigs.")]
         self.rig = None
         self.trace_level = 0
+
+    def acquire_rig(self):
+        self.inventory.remove(CryptoToken("CryptoToken", "Used to acquire rigs."))
+        self.rig = Rig("Hail Mary")
+        print("Rig acquired")
+
+    def launch_data_spikes(self):
