87c094c5ec1b5f4acaaa5ad6d9d38ac821332a6a
Erica Box <boxey001@mymail.unisa.edu.au>
2025-10-23 18:53:48 +1030

    Working through encrypt_assets method.

--- Hacker.py
+++ Hacker.py
@@ -10,6 +10,8 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 from Rig import Rig
 from Asset import CryptoToken
+from Asset import SecurityChip
+from Asset import DataSpike
 
 
 class Hacker:
@@ -24,4 +26,37 @@ class Hacker:
         self.rig = Rig("Hail Mary")
         print("Rig acquired")
 
-    def launch_data_spikes(self):
+    def launch_data_spikes(self, target: Rig):
+        # Ensure target is a Rig instance
+        if not isinstance(target, Rig):
+            print("Target is not a rig")
+        # Consume a DataSpike instance
+        for item in self.inventory:
+            if isinstance(item, DataSpike):
+                self.inventory.remove(item)
+                target.take_hit()
+        print("No DataSpike available.")
+
+
+def encrypt_assets(self):
+    chip_location = None
+    chip = None
+
+    # Check the Hacker's inventory for a SecurityChip
+    for item in self.inventory:
+        if isinstance(item, SecurityChip):
+            chip = item
+            chip_location = "inventory"
+            break
+
+    # If no chip was found in the Hacker's inventory, check the Hacker's rig - if they have one
+    if chip is None and self.rig:
+        for item in self.rig.storage:
+            if isinstance(item, SecurityChip):
+                chip = item
+                chip_location = "rig"
+                break
+
+    if chip is None:
+        print("Security chip needed to encrypt assets.")
+        return
